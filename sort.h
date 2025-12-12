7a79e4566f59a5113e28c0952cfac2d38841860c
Lorenzo Anselme <lorenzoanselme@proton.me>
2025-12-12 11:36:41 +0000

    Fix : Add structure to sort.h

--- sort.h
+++ sort.h
@@ -2,10 +2,24 @@
 #define SORT_H
 
 #include <stddef.h>
-#include <stdio.h>
+
+/**
+ * struct listint_s - Doubly linked list node
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element
+ * @next: Pointer to the next element
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
 
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
+
+void print_list(const listint_t *list);
 void insertion_sort_list(listint_t **list);
 
-#endif /* SORT_H */
+#endif
